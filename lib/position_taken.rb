cc3c1531271ba04ff2d36c29892470d8693c1825
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-26 20:42:35 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
-  elsif board[index] == "X" || == "O"
+  else #board[index] == "X" || == "O"
     return true
   end
 end
